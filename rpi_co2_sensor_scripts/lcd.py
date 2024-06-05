a03789a0f8c14fb5ca845a6f3d01ae60e14bf378
lahzy1 <larpre01@easv365.dk>
2024-06-05 11:26:51 +0200

    Raspberry Pi Folder Files Updated
    I main.py blev post requesten lavet om til en put request.

---
+++ rpi_co2_sensor_scripts/lcd.py
@@ -0,0 +1,192 @@
+#!/usr/bin/python
+#--------------------------------------
+#
+#  lcd2004.py
+#  20x4 LCD Test Script with
+#  backlight control and text justification
+#
+# https://www.sunfounder.com/
+#
+#--------------------------------------
+ 
+# The wiring for the LCD is as follows:
+# 1 : GND
+# 2 : 5V
+# 3 : Contrast (0-5V)*
+# 4 : RS (Register Select)
+# 5 : R/W (Read Write)       - GROUND THIS PIN
+# 6 : Enable or Strobe
+# 7 : Data Bit 0             - NOT USED
+# 8 : Data Bit 1             - NOT USED
+# 9 : Data Bit 2             - NOT USED
+# 10: Data Bit 3             - NOT USED
+# 11: Data Bit 4
+# 12: Data Bit 5
+# 13: Data Bit 6
+# 14: Data Bit 7
+# 15: LCD Backlight +5V**
+# 16: LCD Backlight GND
+ 
+
+# Tutorial og code snippets fra: http://wiki.sunfounder.cc/index.php?title=LCD2004_Module#The_Experiment_for_Raspberry_Pi
+
+# Husk: Brug python3
+import RPi.GPIO as GPIO
+import time
+
+
+class LCD:
+    # Define GPIO to LCD mapping. Remember to not use the TXD0 pin when you just want normal GPIO output pins.
+    LCD_RS = 27
+    LCD_E  = 17
+    LCD_D4 = 18
+    LCD_D5 = 22
+    LCD_D6 = 23
+    LCD_D7 = 24
+    LED_ON = 15
+    
+    # Define some device constants
+    LCD_WIDTH = 20    # Maximum characters per line
+    LCD_CHR = True
+    LCD_CMD = False
+    
+    LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+    LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+    LCD_LINE_3 = 0x94 # LCD RAM address for the 3rd line
+    LCD_LINE_4 = 0xD4 # LCD RAM address for the 4th line
+    
+    # Timing constants
+    E_PULSE = 0.0005
+    E_DELAY = 0.0005
+
+
+    def __init__(self): # This constructor is only needed when you want instance specific code to be executed.
+        # Setup
+        GPIO.setwarnings(False)
+        GPIO.setmode(GPIO.BCM)            # Use BCM GPIO numbers
+        GPIO.setup(self.LCD_E, GPIO.OUT)  # E
+        GPIO.setup(self.LCD_RS, GPIO.OUT) # RS
+        GPIO.setup(self.LCD_D4, GPIO.OUT) # DB4
+        GPIO.setup(self.LCD_D5, GPIO.OUT) # DB5
+        GPIO.setup(self.LCD_D6, GPIO.OUT) # DB6
+        GPIO.setup(self.LCD_D7, GPIO.OUT) # DB7
+        GPIO.setup(self.LED_ON, GPIO.OUT) # Backlight enable
+      
+        # Initialise display
+        self.lcd_init()
+      
+        # TODO: Maybe delete later because I can not see the effect on the display.
+        # Toggle backlight on-off-on
+        # self.lcd_backlight(True)
+        # time.sleep(0.5)
+        # self.lcd_backlight(False)
+        # time.sleep(0.5)
+        # self.lcd_backlight(True)
+        # time.sleep(0.5)
+
+    
+    def lcd_test(self):
+        # Send some test
+        self.lcd_string("Test inside lcd.py", self.LCD_LINE_1, 2)
+        self.lcd_string("RasPiModel 4B", self.LCD_LINE_2, 2)
+        self.lcd_string("20x4 LCD Test", self.LCD_LINE_3, 2)
+        self.lcd_string("SunFounder.com", self.LCD_LINE_4, 2)
+
+        print("Check the LCD display.")
+    
+        time.sleep(2)
+
+
+    def lcd_init(self):
+        # Initialise display
+        self.lcd_byte(0x33, self.LCD_CMD) # 110011 Initialise
+        self.lcd_byte(0x32, self.LCD_CMD) # 110010 Initialise
+        self.lcd_byte(0x06, self.LCD_CMD) # 000110 Cursor move direction
+        self.lcd_byte(0x0C, self.LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
+        self.lcd_byte(0x28, self.LCD_CMD) # 101000 Data length, number of lines, font size
+        self.lcd_byte(0x01, self.LCD_CMD) # 000001 Clear display
+        time.sleep(LCD.E_DELAY)
+    
+    
+    def lcd_byte(self, bits, mode):
+        # Send byte to data pins
+        # bits = data
+        # mode = True  for character
+        #        False for command
+      
+        GPIO.output(self.LCD_RS, mode) # RS
+      
+        # High bits
+        GPIO.output(self.LCD_D4, False)
+        GPIO.output(self.LCD_D5, False)
+        GPIO.output(self.LCD_D6, False)
+        GPIO.output(self.LCD_D7, False)
+        if bits&0x10 == 0x10:
+            GPIO.output(self.LCD_D4, True)
+        if bits&0x20 == 0x20:
+            GPIO.output(self.LCD_D5, True)
+        if bits&0x40 == 0x40:
+            GPIO.output(self.LCD_D6, True)
+        if bits&0x80 == 0x80:
+            GPIO.output(self.LCD_D7, True)
+      
+        # Toggle 'Enable' pin
+        self.lcd_toggle_enable()
+      
+        # Low bits
+        GPIO.output(self.LCD_D4, False)
+        GPIO.output(self.LCD_D5, False)
+        GPIO.output(self.LCD_D6, False)
+        GPIO.output(self.LCD_D7, False)
+        if bits&0x01 == 0x01:
+          GPIO.output(self.LCD_D4, True)
+        if bits&0x02 == 0x02:
+          GPIO.output(self.LCD_D5, True)
+        if bits&0x04 == 0x04:
+          GPIO.output(self.LCD_D6, True)
+        if bits&0x08 == 0x08:
+          GPIO.output(self.LCD_D7, True)
+      
+        # Toggle 'Enable' pin
+        self.lcd_toggle_enable()
+    
+
+    def lcd_toggle_enable(self):
+        # Toggle enable
+        time.sleep(self.E_DELAY)
+        GPIO.output(self.LCD_E, True)
+        time.sleep(self.E_PULSE)
+        GPIO.output(self.LCD_E, False)
+        time.sleep(self.E_DELAY)
+    
+    
+    def lcd_string(self, message, line, style):
+        # Send string to display
+        # style = 1 Left justified
+        # style = 2 Centred
+        # style = 3 Right justified
+      
+        if style == 1:
+            message = message.ljust(self.LCD_WIDTH, " ")
+        elif style == 2:
+            message = message.center(self.LCD_WIDTH, " ")
+        elif style == 3:
+            message = message.rjust(self.LCD_WIDTH, " ")
+      
+        self.lcd_byte(line, self.LCD_CMD)
+      
+        for i in range(self.LCD_WIDTH):
+            self.lcd_byte(ord(message[i]), self.LCD_CHR)
+      
+    
+    def lcd_backlight(self, flag):
+        # Toggle backlight on-off-on
+        GPIO.output(self.LED_ON, flag)
+    
+    
+if __name__ == '__main__':
+    try:
+        lcd = LCD()
+        lcd.lcd_test()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
---
+++ rpi_co2_sensor_scripts/main.py
@@ -0,0 +1,141 @@
+import getpass
+import hashlib
+import hmac
+import requests
+import RPi.GPIO as GPIO
+import time
+from co2_sensor import CO2Sensor
+from lcd import LCD
+
+
+LED_GREEN = 13
+LED_YELLOW = 19
+LED_RED = 26
+BUZZER = 21
+API_ENDPOINT = "https://co2-backend-production.up.railway.app/api/sensor/data"
+
+
+def main():
+    setup_gpio()
+
+    sensor = CO2Sensor("SER14710391759133") # The sensors serial number is needed to identify the sensor in the database.
+    lcd = LCD()
+    buzzer_beeped = False
+    start_time = 0
+    batch_time_interval = 60 # The time interval in seconds for sending data to the server.
+    first_run = True
+    
+    secret_key = getpass.getpass("Enter the secret key: ")#.encode()
+    
+    while True:
+        sensor.measure()
+        
+        print(sensor.data_set)
+        
+        if first_run:
+            token = generate_token(sensor.ID, secret_key)
+            send_sensor_data(API_ENDPOINT, sensor.data_set, token)
+            
+            start_time = time.time()
+            first_run = False
+        
+        if sensor.has_data:
+            lcd.lcd_string("ID:%s" %sensor.ID, lcd.LCD_LINE_1, 1)
+            lcd.lcd_string("CO2: %d ppm" %sensor.CO2, lcd.LCD_LINE_2, 1)
+            lcd.lcd_string("Temp: %0.1f C" %sensor.temperature, lcd.LCD_LINE_3, 1)
+            lcd.lcd_string("Humidity: %0.1f %%" %sensor.relative_humidity, lcd.LCD_LINE_4, 1)
+
+        if sensor.has_data and sensor.CO2 < sensor.yellow_threshold:
+            change_led_state("green")
+            buzzer_beeped = False
+        elif sensor.has_data and sensor.CO2 < sensor.red_threshold:
+            change_led_state("yellow")
+            buzzer_beeped = False
+        elif sensor.has_data and buzzer_beeped == False:
+            change_led_state("red")
+            beep_buzzer(3, 0.05, 0.05)
+            buzzer_beeped = True
+            
+            print("WARNING! CO2 level is above %d ppm. Sending data to server." %sensor.red_threshold)
+            send_sensor_data(API_ENDPOINT, sensor.data_set, token)
+        else:
+            change_led_state("red") 
+            send_sensor_data(API_ENDPOINT, sensor.data_set, token)
+        
+        # For future versions: The sensor unit should send live when a user is watching a live graph through the app/web.
+        if time.time() - start_time >= batch_time_interval:
+            send_sensor_data(API_ENDPOINT, sensor.data_set, token)
+            start_time = time.time()
+           
+
+def generate_token(serial_number, secret_key):
+    timestamp = str(int(time.time()))
+    message = serial_number + timestamp
+    print(f"Message: {message}")
+    signature = hmac.new(secret_key.encode(), message.encode(), hashlib.sha256).hexdigest()
+    print(f"Signature: {signature}")
+    token = f"{timestamp}:{signature}"
+    print(f"Token: {token}")
+    return token
+
+
+def send_sensor_data(url, data_set, token):
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    response = requests.put(url, json=data_set, headers=headers) # The data set dictionary gets automatically converted to JSON format.
+    print(f"Status Code: {response.status_code}")
+    print(f"Response Text: {response.text}")
+
+    try:
+        response_json = response.json()
+        print(response_json)
+    except ValueError:
+        print("Response content is not valid JSON")
+
+
+def setup_gpio():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LED_GREEN, GPIO.OUT)
+    GPIO.setup(LED_YELLOW, GPIO.OUT)
+    GPIO.setup(LED_RED, GPIO.OUT)
+    GPIO.setup(BUZZER, GPIO.OUT)
+    
+    GPIO.output(LED_GREEN, GPIO.LOW)
+    GPIO.output(LED_YELLOW, GPIO.LOW)
+    GPIO.output(LED_RED, GPIO.LOW)
+    GPIO.output(BUZZER, GPIO.LOW)
+                 
+
+def change_led_state(str):
+    if str.lower() == "green":
+        GPIO.output(LED_GREEN, GPIO.HIGH)
+        GPIO.output(LED_YELLOW, GPIO.LOW)
+        GPIO.output(LED_RED, GPIO.LOW)
+    elif str.lower() == "yellow":
+        GPIO.output(LED_GREEN, GPIO.LOW)
+        GPIO.output(LED_YELLOW, GPIO.HIGH)
+        GPIO.output(LED_RED, GPIO.LOW)
+    elif str.lower() == "red":
+        GPIO.output(LED_GREEN, GPIO.LOW)
+        GPIO.output(LED_YELLOW, GPIO.LOW)
+        GPIO.output(LED_RED, GPIO.HIGH)
+    elif str.lower() == "off":
+        GPIO.output(LED_GREEN, GPIO.LOW)
+        GPIO.output(LED_YELLOW, GPIO.LOW)
+        GPIO.output(LED_RED, GPIO.LOW)
+
+
+def beep_buzzer(number_of_times, high_time, low_time):
+    for i in range(number_of_times):
+        GPIO.output(BUZZER, GPIO.HIGH)
+        time.sleep(high_time)
+        GPIO.output(BUZZER, GPIO.LOW)
+        time.sleep(low_time)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
